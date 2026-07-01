37a0eeb27bccc574239f2f76e3cff63e2a14da70
siripong-ctn <siripong.2320@gmail.com>
2026-07-01 21:31:45 +0700

    Add comment toturial or_gate

--- 1OR_GATE/or_gate.vhd
+++ 1OR_GATE/or_gate.vhd
@@ -1,13 +1,19 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-entity or_gate is
+library IEEE; --เรียกใช้ Library IEEE
+use IEEE.STD_LOGIC_1164.ALL; --นำ Package STD_LOGIC_1164 ทั้งหมดมาใช้งาน
+entity or_gate is --ประกาศตัวแปรขาเข้าขาออก
     Port ( 
         A : in  STD_LOGIC;
         B : in  STD_LOGIC;
         Y : out STD_LOGIC
     );
 end or_gate;
-architecture Dataflow of or_gate is
+architecture Dataflow of or_gate is --กำหนดการทำงานของวงจร
 begin
     Y <= A or B;
 end Dataflow;
+-- OR GATE TRUTH TABLE
+--| A | B |(Y)|
+--| 0 | 0 | 0 |
+--| 0 | 1 | 1 |
+--| 1 | 0 | 1 |
+--| 1 | 1 | 1 |
