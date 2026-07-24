5b5c25880033ced87c291e71d57b15d927836537
siripong-ctn <siripong.2320@gmail.com>
2026-07-24 11:18:48 +0700

    Update1 lab2

--- lab2/test.vhd
+++ lab2/test.vhd
@@ -23,45 +23,72 @@ entity test is
         seg_g : out STD_LOGIC
     );
 end test;
+
 architecture Behavioral of test is
-signal A,B,C,D : std_logic;
-signal NA,NB,NC,ND : std_logic;
+
+    -- Encoder Outputs
+    signal A, B, C, D : STD_LOGIC;
+
+    -- Inverted Signals
+    signal NA, NB, NC, ND : STD_LOGIC;
+
+    -- Decoder Outputs
+    signal a_dec, b_dec, c_dec : STD_LOGIC;
+    signal d_dec, e_dec, f_dec : STD_LOGIC;
+    signal g_dec : STD_LOGIC;
+
+    -- Enable Signal (OR 5 Input)
+    signal EN : STD_LOGIC;
 
 begin
+
 --------------------------------------------------
 -- Encoder
 --------------------------------------------------
 
-A <= D8 or D9;
-B <= D4 or D5 or D6 or D7;
-C <= D2 or D3 or D6 or D7;
-D <= D1 or D3 or D5 or D7 or D9;
+    A <= D8 or D9;
+    B <= D4 or D5 or D6 or D7;
+    C <= D2 or D3 or D6 or D7;
+    D <= D1 or D3 or D5 or D7 or D9;
 
 --------------------------------------------------
--- NOT Gate
+-- NOT Gates
 --------------------------------------------------
 
-NA <= not A;
-NB <= not B;
-NC <= not C;
-ND <= not D;
+    NA <= not A;
+    NB <= not B;
+    NC <= not C;
+    ND <= not D;
 
 --------------------------------------------------
--- Decoder
+-- OR 5 Input
+-- D0 + A + B + C + D
 --------------------------------------------------
 
-seg_a <= D0 or A or C or (B and D) or (NB and ND);
+    EN <= D0 or A or B or C or D;
 
-seg_b <= D0 or NB or (C and D) or (NC and ND);
+--------------------------------------------------
+-- Decoder Logic
+--------------------------------------------------
 
-seg_c <= D0 or B or NC or D;
+    a_dec <= A or C or (B and D) or (NB and ND);
+    b_dec <= NB or (C and D) or (NC and ND);
+    c_dec <= B or NC or D;
+    d_dec <= A or (NB and C) or (NB and ND) or (C and ND) or (B and NC and D);
+    e_dec <= (NB and ND) or (C and ND);
+    f_dec <= A or (B and NC) or (C and ND) or (NC and ND);
+    g_dec <= A or (NB and C) or (B and NC) or (B and ND);
 
-seg_d <= D0 or A or (NB and C) or (NB and ND) or (C and ND) or (B and NC and D);
+--------------------------------------------------
+-- Final AND Gates
+--------------------------------------------------
 
-seg_e <= D0 or (NB and ND) or (C and ND);
-
-seg_f <= D0 or A or (B and NC) or (C and ND) or (NC and ND);
-
-seg_g <= A or (NB and C) or (B and NC) or (B and ND);
+    seg_a <= a_dec and EN;
+    seg_b <= b_dec and EN;
+    seg_c <= c_dec and EN;
+    seg_d <= d_dec and EN;
+    seg_e <= e_dec and EN;
+    seg_f <= f_dec and EN;
+    seg_g <= g_dec and EN;
 
 end Behavioral;
